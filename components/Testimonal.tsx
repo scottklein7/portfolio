86e0b2522301f9b93166227037efb3ec0f10a918
scottklein7 <scott.klein02@protonmail.com>
2022-04-18 10:00:48 -0400

    WIP: adds more reviews

--- components/Testimonal.tsx
+++ components/Testimonal.tsx
@@ -15,6 +15,24 @@ function Testimonal() {
                 <p><span className="font-bold">Linda Eddleman</span></p>
                 <p className="text-sm font-extralight">CEO, Trust for the America</p>
             </div>
+            <div className="p-5 flex flex-col gap-3 md:w-1/2 text-emerald-500 border-b border-stone-300">
+                <p>
+                    <span className="text-2xl text-emerald-400">&#8220;</span>
+                    Scott is a very professional web developer who takes pride in his work. He’s always exceeded my expectations; he considers every detail and delivers a high-quality solution and experience.
+                    <span className="text-2xl  text-emerald-400">&#8221;</span>
+                </p>
+                <p><span className="font-bold">Daniel Scott</span></p>
+                <p className="text-sm font-extralight">Owner, Daniel JS Media</p>
+            </div>
+            <div className="p-5 flex flex-col gap-3 md:w-1/2 text-emerald-500 border-b border-stone-300">
+                <p>
+                    <span className="text-2xl text-emerald-400">&#8220;</span>
+                    Scott is an excellent developer with uncanny knack for mastering new programming languages and technologies quickly. His passion and dedication to his craft are evident not only in his code itself but also in his explanations of the ins and outs of his projects.
+                    <span className="text-2xl  text-emerald-400">&#8221;</span>
+                </p>
+                <p><span className="font-bold">Jerome Molasky</span></p>
+                <p className="text-sm font-extralight">Software Engineer</p>
+            </div>
         </div>
 
 
